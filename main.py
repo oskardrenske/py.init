5ad7e5d6410aab846044741fa9f6365f8e272c1c
oskardrenske <oskar.drenske@gmail.com>
2024-12-20 18:10:55 +0100

    updated readme and code examples

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-import os
-from dotenv import load_dotenv, find_dotenv
-
-load_dotenv(find_dotenv())
-
-my_string_env_var = os.getenv("MY_ENV_VAR", "default value")
-
-# strings meet the condition of True if they have content.
-# A comparion of the lower-case-converted string to "true" gives us a boolean value.
-my_bool_env_var = os.getenv("MY_BOOL_ENV_VAR", "").lower() == "true"
+from my_code.environment_variables import my_bool_env_var, my_string_env_var
 
 
 def main():
@@ -15,5 +6,6 @@ def main():
     print(my_bool_env_var)
 
 
+isinstance
 if __name__ == "__main__":
     main()
