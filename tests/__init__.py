aeca861b2c842ead77aae579540dacd7fb9b5ed1
oskardrenske <oskar.drenske@gmail.com>
2024-12-22 10:16:32 +0100

    move code around

---
+++ tests/__init__.py
@@ -0,0 +1,6 @@
+"""
+Den här filen behövs för att importer skall funka.
+
+En nackdel med kod här är att det framgår inte av katalogens filnamn vad som görs var.
+Denna fil kan vara tom eller ha en kilometer kod. Du vet inte förrän du öppnar den.
+"""
